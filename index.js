ce77f859bc686f3f7d31e12bf4dcc49d9f9b9482
Michael Tyler Slaton <mslaton2@uncc.edu>
2019-10-12 17:42:52 -0400

    Initial functionality

---
+++ index.js
@@ -0,0 +1,39 @@
+const Firestore = require('@google-cloud/firestore');
+const PROJECTID = 'hopeful-depot-255718';
+const COLLECTION_NAME = 'posts';
+const firestore = new Firestore({
+    projectId: PROJECTID,
+    timestampsInSnapshots: true,
+    //keyFilename: "/Users/slaton/.cred/wcil-fae725f5f997.json"
+});
+
+exports.posts = (req, res) => {
+    if (req.method === 'POST') {
+        // store/insert a new document
+        const data = (req.body) || {};
+
+        // Only allow valid input values
+        const lat = data.lattitude;
+        const long = data.longitude;
+        const city = data.city;
+        const salary = data.salary;
+        const happiness = data.happiness;
+        const comfort = data.comfort;
+        const created = new Date().getTime();
+
+        // If no data is submitted, don't bother accessing DB
+        if (!data) {
+            return res.status(200)
+        }
+
+        // Create a new firestore collection
+        return firestore.collection(COLLECTION_NAME)
+            .add({ created, lat, long, city, salary, happiness, comfort })
+            .then(doc => {
+                return res.status(200).send(doc);
+            }).catch(err => {
+                console.error(err);
+                return res.status(404).send({ error: 'unable to store', err });
+            });
+    }
+}
